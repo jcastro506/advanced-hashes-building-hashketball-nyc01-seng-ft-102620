f6b47fe717983744c8e07e76311d56b41e054150
Josh Castro <castrojoshua805@gmail.com>
2020-10-18 19:54:26 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,5 +1,16 @@
 # Write your code here!
 def game_hash(hash)
-  
+  new_hash = {
+    :home => {
+      :team_name =>
+      :colors => 
+      :players => 
+    }
+    :away => {
+      :team_name =>
+      :colors => 
+      :players => 
+    }
+  }
 end 
   
