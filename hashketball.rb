c13f5bc894cf6de9ddf7616432c036d9c5af9dbb
Josh Castro <castrojoshua805@gmail.com>
2020-10-18 20:03:41 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,10 +2,18 @@
 def game_hash(hash)
   new_hash = {
     :home => {
-      :team_name => ""
-      :colors => []
+      :team_name => "Brooklyn Nets"
+      :colors => ["Black, White"]
       :players => [{
-        
+        :player_name => "Alan Anderson"
+        :number => 0,
+        :shoe => 16, 
+        :points => 22, 
+        :rebounds => 12, 
+        :assists => 12, 
+        :steals => 3,
+        :blocks => 1, 
+        :slam_dunks => 1 
       }]
     }
     :away => {
